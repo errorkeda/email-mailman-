5981124fe564875f5cd0a0ea47fd9d044dfc45e1
Abhishek Kumar <75356361+errorkeda@users.noreply.github.com>
2022-07-12 18:16:21 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION['email'])) {
-  header("Location:http://localhost/mailman/index.php");
+  header("Location:http://hestalabs.com/tse/email-mailman-/");
 }
 include "autoload.php";
 $gobj = new Database();
