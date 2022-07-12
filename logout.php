44c794804dd40895c33f26c0786ef8cbc6f045af
Abhishek Kumar <75356361+errorkeda@users.noreply.github.com>
2022-07-12 16:59:41 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php   
-session_start(); //to ensure you are using same session
-session_unset();
-session_destroy(); //destroy the session
-header("location:http://localhost/mailman/index.php"); //to redirect back to "index.php" after logging out
-exit();
-?>
